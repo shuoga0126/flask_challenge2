a9ff36b23dae0c7690d77128616558541c52f6eb
shuoga0126 <ogssus0011@gmail.com>
2022-12-02 19:40:40 +0900

    first commit

--- mnist.py
+++ mnist.py
@@ -7,5 +7,5 @@ def hello_world():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 49729))
     app.run(host ='0.0.0.0',port = port)
